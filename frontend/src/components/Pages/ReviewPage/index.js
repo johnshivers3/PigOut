c6d7208bc4d842045fad0e0d12aad6dbed52118a
John Shivers <johnshivers3@gmail.com>
2021-06-23 15:05:29 -0400

    review front

--- frontend/src/components/Pages/ReviewPage/index.js
+++ frontend/src/components/Pages/ReviewPage/index.js
@@ -1,11 +1,12 @@
+import { set } from "js-cookie";
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Redirect, useParams } from "react-router-dom";
 import * as mockData from "../../../assets/SampleDonutData.json";
 
 import "./ReviewPage.css";
 
 export const ReviewPage = () => {
-  const [rating, setRating] = useState();
+  const [rating, setRating] = useState(0);
   const [answer, setAnswer] = useState("");
   const [draft, setDraft] = useState(true);
 
@@ -13,6 +14,15 @@ export const ReviewPage = () => {
   const business = mockData.businesses.find(
     (business) => business.id === businessId
   );
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setDraft(false);
+    console.log({
+      rating,
+      answer,
+    });
+    <Redirect to={`/`} />;
+  };
 
   return (
     <div className="review-main">
@@ -24,7 +34,43 @@ export const ReviewPage = () => {
           <h1>Review</h1>
         </div>
       </div>
-      <h2>{`Let us know what you think of ${business.name}!`}</h2>
+      <div className="review-form-container">
+        <h2>{`Let us know what you think of ${business.name}!`}</h2>
+        <form className="review-form" onSubmit={onSubmit}>
+          <div className="rating-buttons">
+            <div>
+              <label htmlFor="rating">Rating:</label>
+            </div>
+            <button value={1} onClick={(e) => setRating(e.target.value)}>
+              1
+            </button>
+            <button value={2} onClick={(e) => setRating(e.target.value)}>
+              2
+            </button>
+            <button value={3} onClick={(e) => setRating(e.target.value)}>
+              3
+            </button>
+            <button value={4} onClick={(e) => setRating(e.target.value)}>
+              4
+            </button>
+            <button value={5} onClick={(e) => setRating(e.target.value)}>
+              5
+            </button>
+          </div>
+          <div className="answer-div">
+            <div>
+              <label htmlFor="answer">Tell us about your experience:</label>
+            </div>
+            <textarea
+              value={answer}
+              onChange={(e) => setAnswer(e.target.value)}
+              name="answer"
+            />
+          </div>
+          <div></div>
+          <button type="submit">Submit Review</button>
+        </form>
+      </div>
     </div>
   );
 };
