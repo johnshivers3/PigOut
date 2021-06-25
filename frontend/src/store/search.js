2e937ca20271f5393e502ddf5374b32db5c08162
John Shivers <johnshivers3@gmail.com>
2021-06-25 02:53:43 -0400

    re-deploy

---
+++ frontend/src/store/search.js
@@ -0,0 +1,36 @@
+import { csrfFetch } from "./csrf";
+
+const SET_SUGGESTIONS = 'search/SET_SUGGESTIONS'
+
+const setSuggestions = (suggestions) => ({
+  type:SET_SUGGESTIONS,
+  payload: suggestions
+})
+
+export const getSuggestions = (location) => async (dispatch) => {
+  const response = await csrfFetch(`/api/search/suggestions/${location.latitude}/${location.longitude}`)
+  if (response.ok) {
+    const {suggestions} = await response.json();
+    await dispatch(setSuggestions(suggestions));
+    return suggestions;
+  } else {
+    throw new Error("Resource not found.")
+  }
+};
+
+const initialState = {};
+
+const searchReducer = (state = initialState, action) => {
+  let newState;
+
+  switch (action.type) {
+    case SET_SUGGESTIONS:
+      newState = Object.assign({}, state);
+      newState.suggestions = action.payload;
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default searchReducer;
