71bd73d3983a4f26eba45f23514946173074e111
John Shivers <johnshivers3@gmail.com>
2021-06-25 19:51:12 -0400

    EOD Friday

--- frontend/src/components/Content/SplashHeader/SearchBar/index.js
+++ frontend/src/components/Content/SplashHeader/SearchBar/index.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { PreCategories } from "../PreCatgories";
 import * as sessionActions from "../../../../store/session";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { Autocomplete } from "@react-google-maps/api";
 
 import "./SearchBar.css";
@@ -20,21 +21,29 @@ export const SearchBar = () => {
           {/* <Autocomplete
           apiKey={process.env.REACT_APP_GOOGLE_KEY}
           > */}
-            <input name="term" placeholder="Find somewhere to PigOut" />
+          <input name="term" placeholder="Find somewhere to PigOut"></input>
+
           {/* </Autocomplete> */}
         </div>
         <div>
           <label htmlFor="location">LOCATION</label>
 
-          <input name="location" placeholder="Hockessin, DE" />
+          <input
+            name="location"
+            placeholder={myLocation ? "Current Location" : "Search by Location"}
+          ></input>
+
           <button
-            onClick={(e) => {
+            onClick={async (e) => {
               e.preventDefault();
-              setMyLocation(true);
+              await setMyLocation(true);
             }}
             type="button"
           >
             <i className="fas fa-map-marker-alt"></i>
+          </button>
+          <button className="search-button">
+            <i className="fas fa-search"></i>
           </button>
         </div>
       </div>
--- frontend/src/components/Pages/ProfilePage/index.js
+++ frontend/src/components/Pages/ProfilePage/index.js
@@ -1,11 +1,24 @@
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-
+import SuggestionCards from '../../Content/SuggestionsContainer/SuggestionsCards'
+import './ProfilePage.css'
 export const ProfilePage = () => {
   const state = useSelector(state=>state)
-  console.log(state);
+
   return (
-    null
+    <div className='profile-main-container'>
+      <div className='profile-left-side-bar'>
+        <button>Manage Profile</button>
+        <button>Your Check-Ins</button>
+        <button>Your Reviews</button>
+        <button>Your Saved Businesses</button>
+        <button>Your Collections</button>
+      </div>
+      <div className='profile-main-content'></div>
+      <div className='profile-right-side-bar'>
+
+      </div>
+    </div>
   )
 }
 
